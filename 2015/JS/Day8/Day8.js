bde82d77e5853762986c2f2cf97ce7546c4a5664
Lucija Vusić <vusic.lucija@gmail.com>
2019-07-10 00:07:36 +0200

    day 8_1

---
+++ 2015/JS/Day8/Day8.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var characters  = fs.readFileSync('Day8/input.txt').toString().split("\n");
+
+var num_of_char = characters.map(x => x.length).reduce((a,b) => a + b, 0);
+var num_of_mem  = characters.map(x => eval(x).length).reduce((a,b) => a + b, 0);
+
+console.log(num_of_char - num_of_mem);
+
+
+
